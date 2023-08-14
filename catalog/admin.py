1684f37ce132e8cb04b7e67e17c5f41f54414fb4
Sherika91 <shermadinil.luka@gmail.com>
2023-08-14 18:26:29 +0400

    minor changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Student
+
+# admin.site.register(Students)
+
+
+@admin.register(Student)
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('name', 'surname',)
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+NULL_BLANK = {'null': True, 'blank': True}
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100, verbose_name='First Name')
+    surname = models.CharField(max_length=100, verbose_name='Last Name')
+    avatar = models.ImageField(upload_to='students/', verbose_name='Avatar', **NULL_BLANK,)
+    age = models.PositiveSmallIntegerField(verbose_name='Age', **NULL_BLANK,)
+
+    is_active = models.BooleanField(default=True, verbose_name='studies now',)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+        ordering = ('surname',)
